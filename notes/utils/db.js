4ce38b19716945bc89abee69af6124a3df49e882
Nyukaa <annashitikova@gmail.com>
2026-03-15 12:27:43 +0200

    Note Add database migration setup

--- notes/utils/db.js
+++ notes/utils/db.js
@@ -1,5 +1,5 @@
 const { Sequelize } = require("sequelize");
-
+const { Umzug, SequelizeStorage } = require("umzug");
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   dialectOptions: {
@@ -9,9 +9,26 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     },
   },
 });
+const runMigrations = async () => {
+  const migrator = new Umzug({
+    migrations: {
+      glob: "migrations/*.js",
+    },
+    storage: new SequelizeStorage({ sequelize, tableName: "migrations" }),
+    context: sequelize.getQueryInterface(),
+    logger: console,
+  });
+
+  const migrations = await migrator.up();
+  console.log("Migrations up to date", {
+    files: migrations.map((mig) => mig.name),
+  });
+};
+
 const connectToDatabase = async () => {
   try {
     await sequelize.authenticate();
+    await runMigrations();
     console.log("connected to the database");
   } catch (err) {
     console.log("failed to connect to the database");
